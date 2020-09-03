7d29c8ab76269c9fdaa631a28a3773dc8fcce3e2
nkholane <nkholane@student.wethinkcode.co.za>
2020-09-04 01:03:52 +0200

    deleted old folder

--- avaj/Flyable.java
+++
@@ -1,9 +0,0 @@
-package dev.avaj;
-
-import dev.avaj.tower.WeatherTower;
-
-public interface Flyable {
-    public void updateConditions();
-    public void registerTower(WeatherTower WeatherTower);
-}
- // This section is done
